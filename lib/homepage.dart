8f4b2cf08cada51d0325659bd21368154819179c
Riddhisf <riddhisaraf8@gmail.com>
2024-03-25 13:30:37 +0530

    added navbar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = 'home_page';
@@ -11,27 +13,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Widget> cards = []; // List to hold cards
+  final _pageController = PageController(initialPage: 0);
+  final _controller = NotchBottomBarController(index: 0);
+
+  int maxCount = 2;
 
   @override
-  void initState() {
-    super.initState();
-    cards.add(
-      Card(
-        margin: EdgeInsets.all(16.0),
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.add,
-            ),
-            color: Colors.deepPurple[400],
-          ),
-        ),
-      ),
-    );
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
+
+  /// widget list
+  final List<Widget> bottomBarPages = [
+    General(),
+    const Favorite(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -57,10 +54,112 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: GridView.count(
-        crossAxisCount: 2, // Number of columns in the grid
-        children: cards, // Display cards from the list
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+          bottomBarPages.length,
+          (index) => bottomBarPages[index],
+        ),
+      ),
+      extendBody: false,
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              notchBottomBarController: _controller,
+              color: Colors.deepPurple.shade100,
+              showLabel: false,
+              shadowElevation: 0,
+              kBottomRadius: 10.0,
+              notchColor: Colors.deepPurple.shade300,
+              removeMargins: false,
+              bottomBarWidth: MediaQuery.of(context).size.width,
+              showShadow: true,
+              durationInMilliSeconds: 300,
+              elevation: 0,
+              bottomBarItems: const [
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.white,
+                  ),
+                  activeItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.white,
+                  ),
+                  itemLabel: 'General',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.star,
+                    color: Colors.white,
+                  ),
+                  activeItem: Icon(
+                    Icons.star,
+                    color: Colors.white,
+                  ),
+                  itemLabel: 'Favorite',
+                ),
+              ],
+              onTap: (index) {
+                /// perform action on tab change and to update pages you can update pages without pages
+                log('current selected index $index');
+                _pageController.jumpToPage(index);
+              },
+              kIconSize: MediaQuery.of(context).size.height * 0.045,
+            )
+          : null,
+    );
+  }
+}
+
+class General extends StatefulWidget {
+  const General({Key? key}) : super(key: key);
+
+  @override
+  State<General> createState() => _GeneralState();
+}
+
+class _GeneralState extends State<General> {
+  List<Widget> cards = [];
+  // List to hold cards
+
+  @override
+  void initState() {
+    super.initState();
+    cards.add(
+      Card(
+        elevation: 0,
+        color: Colors.deepPurple.shade50,
+        margin: EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add,
+            ),
+            color: Colors.deepPurple[400],
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2, // Number of columns in the grid
+      children: cards, // Display cards from the list
+    );
+  }
+}
+
+class Favorite extends StatelessWidget {
+  const Favorite({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white, child: const Center(child: Text('Favorite')));
   }
 }
